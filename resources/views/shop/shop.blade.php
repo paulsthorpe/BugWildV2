087094d80bcc7ac4179f6dc3bc337c04573da8aa
paulsthorpe <paulsthorpe@gmail.com>
2016-06-27 22:13:13 -0400

    align paginate buttons fr this time

--- resources/views/shop/shop.blade.php
+++ resources/views/shop/shop.blade.php
@@ -51,8 +51,8 @@ Flash messages to report errors to customers.
                         </div>
                     @endforeach
                 @endif
+                {{ $products->links() }}
             </div>
-            {{ $products->links() }}
         </div>
     </div>
     </body>
